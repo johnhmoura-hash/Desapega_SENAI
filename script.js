be0c08ee78f24f63cdd30acdf0f95658f01b1e6c
johnhmoura-hash <john.h.moura@aluno.senai.br>
2026-02-19 11:13:36 -0300

    novo

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 /* Tela de Login*/
+const nome = document.getElementById('usuario'); 
+const senha = document.getElementById('senha');
 
 function validarFormulario() {
-    var nome = document.getElementById('usuario').value;
-    var senha = document.getElementById('senha').value;
     
     if (nome.trim()=="" || senha.trim()=="") {
         alert('Por favor, preencha todos os campos obrigatórios.');
@@ -12,32 +12,45 @@ function validarFormulario() {
         alert('');
         return false;
     }
-
     return true;
 }
 
-let bnt_eye = document.querySelector('#versenha');
+const bnt_eye = document.querySelector('#versenha');
 
+if(bnt_eye){
 bnt_eye.addEventListener('click', ()=>{
-    let inputSenha = document.querySelector('#senha');
+    const inputSenha = document.querySelector('#senha');
 
     if(inputSenha.getAttribute('type') == 'password'){
        inputSenha.setAttribute('type', 'text');
+       bnt_eye.classList.remove('ri-eye-line');
+       bnt_eye.classList.add('ri-eye-off-line');
+
     }else{
         inputSenha.setAttribute('type', 'password');
+        bnt_eye.classList.remove('ri-eye-off-line');
+        bnt_eye.classList.add('ri-eye-line');
     }
 })
-let bnt_eyeConfirm = document.querySelector('#verconfirmesenha');
+}
 
+const bnt_eyeConfirm = document.querySelector('#verconfirmesenha');
+
+if(bnt_eyeConfirm){
 bnt_eyeConfirm.addEventListener('click', ()=>{
     let inputSenhaConfirm = document.querySelector('#confirmarSenha');
 
     if(inputSenhaConfirm.getAttribute('type') == 'password'){
        inputSenhaConfirm.setAttribute('type', 'text');
+        bnt_eyeConfirm.classList.remove('ri-eye-line');
+        bnt_eyeConfirm.classList.add('ri-eye-off-line');
     }else{
         inputSenhaConfirm.setAttribute('type', 'password');
+        bnt_eyeConfirm.classList.remove('ri-eye-off-line');
+        bnt_eyeConfirm.classList.add('ri-eye-line');
     }
 })
+}
 
 /* slider */
 
@@ -46,7 +59,7 @@ let btnNav = document.querySelectorAll('.btn-nav-box .btn-nav');
 
 let contadorImg = imgSlider.length;
 let imgAtiva = 0;
-
+if(imgSlider){
 function mostrarSlider(){
 
     let antigaImg = document.querySelector('.slider-container .slider-box.ativo')
@@ -79,4 +92,4 @@ btnNav.forEach((btn, indice)=>{
 });
 
 iniciarAutoPlay();
-
+}
