ea2f3b35aae3baca625b983de945db4785454729
viniciusmenezes-beep <vinicius.menezes@aluno.senai.br>
2026-03-03 11:13:36 -0300

    novo

--- script.js
+++ script.js
@@ -604,3 +604,24 @@ botao.onclick = () => {
   botao.classList.toggle("selecionado");
 }
 }
+
+
+
+
+
+
+
+
+  const toggle = document.getElementById("menuToggle");
+  const menu = document.getElementById("menu");
+  const over = document.getElementById("overlayMenu");
+
+  toggle.addEventListener("click", () => {
+    menu.classList.toggle("ativo");
+    overlay.classList.toggle("ativo");
+  });
+
+  overlay.addEventListener("click", () => {
+    menu.classList.remove("ativo");
+    overlay.classList.remove("ativo");
+  });
