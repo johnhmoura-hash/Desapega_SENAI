9bf335a668a23db6b1d7f1edbdc6ffbd306d31f8
johnhmoura-hash <john.h.moura@aluno.senai.br>
2026-03-03 19:11:23 -0300

    atualização
    coloquei as categorias em carrossel e mudei os cards para que fiquem responsivo

--- resp.js
+++ resp.js
@@ -2,7 +2,7 @@
   const toggle = document.getElementById("menuToggle");
   const menu = document.getElementById("menu");
   const over = document.getElementById("overlayMenu");
-
+if(over){
   toggle.addEventListener("click", () => {
     menu.classList.toggle("ativo");
     overlay.classList.toggle("ativo");
@@ -12,3 +12,45 @@
     menu.classList.remove("ativo");
     overlay.classList.remove("ativo");
   });
+}
+
+const slider = document.querySelector('.ajuste');
+if(slider){
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+});
+
+slider.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 2;
+  slider.scrollLeft = scrollLeft - walk;
+});
+
+/* suporte a toque */
+slider.addEventListener('touchstart', (e) => {
+  startX = e.touches[0].pageX;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('touchmove', (e) => {
+  const x = e.touches[0].pageX;
+  const walk = (x - startX);
+  slider.scrollLeft = scrollLeft - walk;
+});
+}
