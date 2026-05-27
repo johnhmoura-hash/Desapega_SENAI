a0ac7a6462c44b3afdfa7e337410ad3c263978ab
johnhmoura-hash <john.h.moura@aluno.senai.br>
2026-05-27 09:25:13 -0300

    teste

--- DesapegaSenai/DesapegaSenai/Models/Usuario.cs
+++ DesapegaSenai/DesapegaSenai/Models/Usuario.cs
@@ -2,5 +2,6 @@
 {
     public class Usuario
     {
+        public int Id { get; set; } 
     }
 }
--- DesapegaSenai/DesapegaSenai/obj/Debug/net8.0/DesapegaSenai.AssemblyInfo.cs
+++ DesapegaSenai/DesapegaSenai/obj/Debug/net8.0/DesapegaSenai.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesapegaSenai")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7e253c18819e803134d0cafd1ac1cf282768a0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesapegaSenai")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesapegaSenai")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
