25f377ec133c96d27734945a50c6f261c9547fc7
viniciusmenezes-beep <vinicius.menezes@aluno.senai.br>
2026-06-01 09:43:52 -0300

    a

--- DesapegaSenai/DesapegaSenai/Controllers/TrocasController.cs
+++ DesapegaSenai/DesapegaSenai/Controllers/TrocasController.cs
@@ -6,6 +6,7 @@ using DesapegaSenai.Models;
 namespace DesapegaSenai.Controllers
 {
 
+
     [ApiController]
     [Route("[controller]")]
     public class TrocasController : ControllerBase
@@ -18,24 +19,51 @@ namespace DesapegaSenai.Controllers
             _context = context;
         }
 
+
         [HttpGet()]
         public IActionResult Trocas()
         {
             return Ok(_context.Trocas.ToList());
         }
 
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            var troca = _context.Trocas.Find(id);
+
+            if (troca == null)
+                return NotFound();
+
+            return Ok(troca);
+        }
+
         [HttpPost]
         public IActionResult Trocar(Troca item)
         {
-
-            var usuario = HttpContext.Session.GetString("Email");
-            if (usuario == null)
+            var email = HttpContext.Session.GetString("Email");
+            if (email == null)
                 return Unauthorized("Não autenticado");
 
-            var idUsuarioLogado = Request.Cookies["IdUsado"];
-            if (idUsuarioLogado != null)
+            var usuario = _context.Usuarios
+                .FirstOrDefault(u => u.Email == email);
 
-                _context.Trocas.Add(item);
+            if (usuario == null)
+                return Unauthorized();
+
+            item.Fk_usuarios_remetente = usuario.Matricula;
+            item.Data = DateOnly.FromDateTime(DateTime.Now);
+            item.Status = "Pendente";
+
+            if (item.Fk_usuarios_destinatario == 0)
+                return BadRequest("Destinatário não informado.");
+
+            if (item.Fk_objeto_remetente == 0)
+                return BadRequest("Objeto remetente não informado.");
+
+            if (item.Fk_objeto_destinatario == 0)
+                return BadRequest("Objeto destinatário não informado.");
+
+            _context.Trocas.Add(item);
             _context.SaveChanges();
 
             return Ok(item);
@@ -44,6 +72,10 @@ namespace DesapegaSenai.Controllers
         [HttpDelete("{id}")]
         public IActionResult Deletar(int id)
         {
+            var usuario = HttpContext.Session.GetString("Email");
+            if (usuario == null)
+                return Unauthorized("Não autenticado");
+
             var item = _context.Trocas.Find(id);
 
             if (item == null)
