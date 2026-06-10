9d11d55fa46ab34ad047dacd739ca99cf1c281bc
leticiadoria-prog <leticia.doria@aluno.senai.br>
2026-06-10 09:43:46 -0300

    a

--- DesapegaSenai/DesapegaSenai/obj/Debug/net8.0/DesapegaSenai.AssemblyInfo.cs
+++ DesapegaSenai/DesapegaSenai/obj/Debug/net8.0/DesapegaSenai.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesapegaSenai")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+324d26e9af294c07cb9ab888cad85ae6aa89ff78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adc777d4aebd2a7d2487f1e3ab8fc0d8084d5992")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesapegaSenai")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesapegaSenai")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
