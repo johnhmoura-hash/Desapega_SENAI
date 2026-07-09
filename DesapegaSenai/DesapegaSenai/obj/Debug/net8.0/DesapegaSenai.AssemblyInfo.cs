3f22ee32a6354f7c0ffa43f9e6de9e4a51fee91a
viniciusmenezes-beep <vinicius.menezes@aluno.senai.br>
2026-07-09 07:48:21 -0300

    novo

--- DesapegaSenai/DesapegaSenai/obj/Debug/net8.0/DesapegaSenai.AssemblyInfo.cs
+++ DesapegaSenai/DesapegaSenai/obj/Debug/net8.0/DesapegaSenai.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesapegaSenai")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c41f554f49358ae446c7bd062a1724a0fc4e0e88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21909803402911858ab14dea3798408fabfcd7ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesapegaSenai")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesapegaSenai")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
