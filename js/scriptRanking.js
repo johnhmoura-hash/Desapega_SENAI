6f439798dc3736dcbb53c1da30260ce64de7090f
johnhmoura-hash <john.h.moura@aluno.senai.br>
2026-07-15 11:07:19 -0300

    atualizei o ranking

--- js/scriptRanking.js
+++ js/scriptRanking.js
@@ -1,26 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-fetch('https://localhost:7132/objeto/ranking', {
+    fetch('https://localhost:7132/objeto/ranking', {
         credentials: "include"
     })
-    .then(response => response.json())
-    .then(data => {
+        .then(response => response.json())
+        .then(data => {
 
-         const resposta= document.getElementById("ranking");
-        if (!resposta) {
-            console.error("Elemento #info não existe no HTML");
-            return;
-        }
+            const resposta = document.getElementById("ranking");
+            if (!resposta) {
+                console.error("Elemento #info não existe no HTML");
+                return;
+            }
 
-        
-        resposta.innerHTML = "";
-        
-         for (let i = 0; i < data.length; i++) {
-            
-            resposta.innerHTML += `
+
+            resposta.innerHTML = "";
+
+            for (let i = 0; i < data.length; i++) {
+
+                resposta.innerHTML += `
                 
             <div class="ranking-item ranking-gold">
-            <span class="ranking-position">#${[i+1]}</span>
+            <span class="ranking-position">#${[i + 1]}</span>
             <span class="ranking-name">${data[i].nome}</span>
             <span class="ranking-value">${data[i].totalObjetos}</span>
             <span class="ranking-value">${data[i].totalTrocas}</span>
@@ -29,8 +29,8 @@ fetch('https://localhost:7132/objeto/ranking', {
 
                       
             `;
-         }
+            }
 
-            
-    });
+
+        });
 })
